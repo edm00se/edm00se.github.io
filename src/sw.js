4287bc47b57f9be5645c65dcd4ab4700b9021abf
Eric McCormick <edm00se@gmail.com>
2018-11-03 20:21:10 -0500

    fix(sw): removes console log from sw

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,6 @@
 // stub for prod sw w/ parcel
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
-console.log('this is my custom service worker');
-
 workbox.precaching.precacheAndRoute([]);
 
 workbox.routing.registerRoute(
