f424e30c7b483fada4d82240a0f9eac523527e64
Eric McCormick <edm00se@users.noreply.github.com>
2019-02-06 15:22:21 -0600

    corrects a typo
    ask me, not ask my

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -2,8 +2,8 @@
   <div class="links">
     <div class="link" v-for="link in links" :key="link.href">
       <a class="link-of-links" :href="link.href">
-        <h3>{{link.title}}</h3>
-        {{link.descrip}}
+        <h3>{{ link.title }}</h3>
+        {{ link.descrip }}
       </a>
     </div>
   </div>
@@ -47,7 +47,7 @@ export default {
         {
           title: 'ama',
           href: 'https://github.com/edm00se/ama/',
-          descrip: `ask my (nearly) anything`
+          descrip: `ask me (nearly) anything`
         }
       ]
     };
@@ -56,64 +56,66 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.links {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  align-self: auto;
+  transition: 0.5s;
+  right: 0;
+}
+a.link-of-links {
+  color: black;
+  position: relative;
+}
+a.link-of-links:hover {
+  color: #d07922;
+}
+a.link-of-links:before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: #d07922;
+  visibility: hidden;
+  -webkit-transform: scaleX(0);
+  transform: scaleX(0);
+  -webkit-transition: all 0.3s ease-in-out 0s;
+  transition: all 0.3s ease-in-out 0s;
+}
+a.link-of-links:hover:before {
+  visibility: visible;
+  -webkit-transform: scaleX(1);
+  transform: scaleX(1);
+}
+.link {
+  padding-top: 0.25rem;
+  padding-bottom: 0.25rem;
+  animation-name: fade-in;
+  animation-fill-mode: both;
+  animation-duration: 0.5s;
+}
+.link-of-links {
+  display: block;
+  text-decoration: none;
+}
 
-  .links {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    align-self: auto;
-    transition: 0.5s;
-    right: 0;
+$animationDelay: 1;
+@for $i from 1 through 15 {
+  .link:nth-of-type(#{$i}) {
+    animation-delay: #{0.3+ ($i)/30}s;
   }
-  a.link-of-links {
-    color: black;
-    position: relative;
-  }
-  a.link-of-links:hover {
-    color: #d07922;
-  }
-  a.link-of-links:before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: #d07922;
-    visibility: hidden;
-    -webkit-transform: scaleX(0);
-    transform: scaleX(0);
-    -webkit-transition: all 0.3s ease-in-out 0s;
-    transition: all 0.3s ease-in-out 0s;
-  }
-  a.link-of-links:hover:before {
-    visibility: visible;
-    -webkit-transform: scaleX(1);
-    transform: scaleX(1);
-  }
-  .link {
-    padding-top: 0.25rem;
-    padding-bottom: 0.25rem;
-    animation-name: fade-in;
-    animation-fill-mode: both;
-    animation-duration: 0.5s;
-  }
-  .link-of-links {
-    display: block;
-    text-decoration: none;
-  }
+}
 
-  $animationDelay: 1;
-  @for $i from 1 through 15 {
-    .link:nth-of-type(#{$i}) {
-      animation-delay: #{0.3+($i)/30}s;
-    }
+@keyframes fade-in {
+  0% {
+    opacity: 0;
   }
-
-  @keyframes fade-in {
-    0% { opacity: 0; }
-    100% { opacity: 1; }
+  100% {
+    opacity: 1;
   }
-
+}
 </style>
