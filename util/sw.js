da2e4ebff5acec99ccb3b1d234f3c6e89d4d979b
Eric McCormick <edm00se@gmail.com>
2021-04-22 15:57:49 -0500

    implements workbox-build with network first strategy and pre-cached assets

---
+++ util/sw.js
@@ -0,0 +1,31 @@
+// update this release when updating workbox-build
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js'
+);
+
+workbox.setConfig({
+  debug: true
+});
+
+// workbox.googleAnalytics.initialize();
+
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+workbox.routing.registerRoute(
+  ({ url }) => url.pathname.startsWith('/'),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://api.github.com/users/edm00se'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'github-user-api'
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://avatars3.githubusercontent.com/u/622118?v=4'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'github-user-profile-image'
+  })
+);
