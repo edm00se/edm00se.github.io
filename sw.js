bc147b02b1c5f21c010bce4d2061c904746da750
edm00se <edm00se@users.noreply.github.com>
2022-11-07 22:51:46 +0000

    Deploying to master from @ edm00se/edm00se.github.io@fe19361c918b8d8cf97e38f42ee88f1c77bb3332 🚀

--- sw.js
+++ sw.js
@@ -1,31 +1,13 @@
-// update this release when updating workbox-build
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js'
-);
-
-workbox.setConfig({
-  debug: false
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
 });
 
-// workbox.googleAnalytics.initialize();
-
-workbox.precaching.precacheAndRoute([{"revision":"41d6952060d18d28ca0c16a07433b43a","url":"404.html"},{"revision":"352507aeea9d8e0c83a082c9c69511fc","url":"assets/index.952da9e9.js"},{"revision":"35a4fb216cf7f4eb03b4d907620b92b9","url":"assets/index.dd7455d7.css"},{"revision":"f902d715faa9a497b0babf8ac8c37221","url":"icons/114X114-icon.png"},{"revision":"51f9624739569b0e47d484e793e2d2ef","url":"icons/144X144-icon.png"},{"revision":"cfed735b581f09f437db2b05a9fb637f","url":"icons/512X512-icon.png"},{"revision":"d89a4e1a8200aa8506caffd810dfb328","url":"icons/57X57-icon.png"},{"revision":"3f0083b32c218bd34db4c003574e4df9","url":"icons/72X72-icon.png"},{"revision":"0b102a3cd0e547edd1b598b10680af30","url":"icons/favicon.ico"},{"revision":"de2a882f928ce7c71796685f17aa434c","url":"icons/favicon.png"},{"revision":"a72eff01489f809f52f60904bac5c20c","url":"index.html"}]);
-
-workbox.routing.registerRoute(
-  ({ url }) => url.pathname.startsWith('/'),
-  new workbox.strategies.NetworkFirst()
-);
-
-workbox.routing.registerRoute(
-  new RegExp('https://api.github.com/users/edm00se'),
-  new workbox.strategies.CacheFirst({
-    cacheName: 'github-user-api'
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp('https://avatars3.githubusercontent.com/u/622118?v=4'),
-  new workbox.strategies.CacheFirst({
-    cacheName: 'github-user-profile-image'
-  })
-);
+self.addEventListener('activate', function(e) {
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      clients.forEach(client => client.navigate(client.url))
+    });
+});
