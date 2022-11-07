ff500e06df88a07fcfd15658652cdcfedbd90f97
Eric McCormick <edm00se@users.noreply.github.com>
2022-11-07 16:53:06 -0600

    removes sw registration from main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,3 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
-
-if (import.meta.env.PROD && 'serviceWorker' in window.navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((reg) => console.log('service worker registerd successfully'))
-      .catch((err) => console.error(err));
-  });
-}
